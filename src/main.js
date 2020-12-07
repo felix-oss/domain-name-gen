91d75c3ce6e5f475c32c9017d737a948c797aba9
felix-oss <uv.mhjl@gmail.com>
2020-12-07 19:15:25 +0000

    update

--- src/main.js
+++ src/main.js
@@ -4,18 +4,17 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-import { Button } from "bootstrap";
 
 let pronoun = ["the", "our"];
-let adj = ["great", "big"];
-let noun = ["jogger", "racoon"];
-let com = [".com", ".io"];
+let adj = ["great", "big", "crush"];
+let noun = ["jogger", "racoon", "car", "domain"];
+let com = [".com", ".io", ".xyz", ".es"];
 
 window.domnames = function domnames() {
   for (var i = 0; i < pronoun.length; i++) {
     for (var a = 0; a < adj.length; a++) {
       for (var b = 0; b < noun.length; b++) {
-        for (var c = 0; c < noun.length; c++) {
+        for (var c = 0; c < com.length; c++) {
           document.querySelector("#results").innerHTML +=
             "<li>" + pronoun[i] + adj[a] + noun[b] + com[c] + "</li>";
         }
