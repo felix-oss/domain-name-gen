a27982de0fd656066b246e8058a8510bbfa6bc04
felix-oss <uv.mhjl@gmail.com>
2020-12-07 15:57:26 +0000

    initial

--- src/main.js
+++ src/main.js
@@ -5,6 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let com = [".com", ".io"];
+
+window.domnames = function domnames() {
+  for (var i = 0; i < pronoun.length; i++) {
+    for (var a = 0; a < adj.length; a++) {
+      for (var b = 0; b < noun.length; b++) {
+        for (var c = 0; c < noun.length; c++) {
+          document.querySelector("#results").innerHTML +=
+            "<li>" + pronoun[i] + adj[a] + noun[b] + com[c] + "</li>";
+        }
+      }
+    }
+  }
 };
